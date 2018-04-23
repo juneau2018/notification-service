2f74fe2fba607beb9ed5fc381cdbab4aa95d095a
詹叶 <zhany@HeisenbergMAC.local>
2018-04-24 00:59:49 +0800

    add

---
+++ src/test/java/com/piggymetrics/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.piggymetrics.notification;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = NotificationServiceApplication.class)
+@WebAppConfiguration
+public class NotificationServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
